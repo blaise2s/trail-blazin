716c4c9053b2b3a5faa8d79ebc3e4f032c879495
blaise2s <blaise.schaeffer@gmail.com>
2020-12-07 23:10:39 -0500

    feat(TB-1): Adds angular meterial and creates main page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BakingComponent } from './baking/baking.component';
+import { CyclingComponent } from './cycling/cycling.component';
+import { GardeningComponent } from './gardening/gardening.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'cycling',
+    component: CyclingComponent
+  },
+  {
+    path: 'gardening',
+    component: GardeningComponent
+  },
+  {
+    path: 'baking',
+    component: BakingComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/baking/baking.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tb-baking',
+  templateUrl: './baking.component.html',
+  styleUrls: ['./baking.component.scss']
+})
+export class BakingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
